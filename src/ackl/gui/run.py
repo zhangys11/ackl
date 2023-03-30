9918a4d78c4ef0c920e4895e73850211dc0ba684
zhangys11 <92197345+zhangys11@users.noreply.github.com>
2023-03-30 23:10:03 +0800

    fix file upload problem

--- src/ackl/gui/run.py
+++ src/ackl/gui/run.py
@@ -16,7 +16,7 @@ else:
     import metrics
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 5 * 1024 * 1024  # limit to 5MB
+app.config['MAX_CONTENT_LENGTH'] = 50 * 1024 * 1024  # limit to 50MB, to avoid 413 error.
 
 def load_file(pathname):
     '''
@@ -59,36 +59,30 @@ def index():
     return render_template("home.html")
 
 
-@app.route("/submit", methods=['GET', 'POST'])
-def run_ackl():    
-    if request.method == 'POST':
+@app.route("/submit", methods=['POST'])
+def run_ackl():
+    r = ''
+    kernel_type = request.form['kernel_type']
+    use_sample = request.form["use_sample"]
 
-        r = ''
-        kernel_type = request.form['name']
-        print(kernel_type)
-        use_sample = request.form["use_sample"]
-        print(use_sample)
-
-        if (use_sample):
-            # path = "ackl/gui/static/sample.csv"
-            X, y, X_names, desc, labels = io.load_dataset('salt', x_range=list(range(400, 1000)), display=False)
-            pkl = metrics.preview_kernels(X, y, metrics=True,
-                                          scatterplot=False,
-                                          selected_kernel_names= [kernel_type])
-            r = metrics.visualize_metric_dicts(pkl, plot=False)
-        else:
-            f = request.files['dataFile']
-            csv = os.path.dirname(os.path.realpath(
-                __file__)) + "/" + str(uuid.uuid4()) + ".csv"
-            f.save(csv)
-            print('1')
-            X,y = analyze_file(csv)
-            pkl = metrics.preview_kernels(X, y,metrics=True,
-                                          scatterplot=False,
-                                          selected_kernel_names= [kernel_type])
-            r = metrics.visualize_metric_dicts(pkl, plot=False)
-
-
+    if (use_sample == 'true' or use_sample == True):
+        # path = "ackl/gui/static/sample.csv"
+        X, y, X_names, desc, labels = io.load_dataset('salt', x_range=list(range(400, 1000)), display=False)
+        pkl = metrics.preview_kernels(X, y, metrics=True,
+                                        scatterplot=False,
+                                        selected_kernel_names= [kernel_type])
+        r = metrics.visualize_metric_dicts(pkl, plot=False)
+    else:
+        f = request.files['dataFile']
+        csv = os.path.dirname(os.path.realpath(
+            __file__)) + "/" + str(uuid.uuid4()) + ".csv"
+        f.save(csv)
+        print('1')
+        X,y = analyze_file(csv)
+        pkl = metrics.preview_kernels(X, y,metrics=True,
+                                        scatterplot=False,
+                                        selected_kernel_names= [kernel_type])
+        r = metrics.visualize_metric_dicts(pkl, plot=False)
     return {'message': 'success', 'html': r}
     #render_template("home.html", use_sample = use_sample,  cla_result = r)
 
@@ -96,6 +90,7 @@ def run_ackl():
 #    webbrowser.open_new('http://localhost:5052/')
 
 if __name__ == '__main__':
+
     # # use netstat -ano|findstr 5051 to check port use
     # Timer(3, open_browser).start()
     # app.run(host="0.0.0.0", port=5052, debug=False)
